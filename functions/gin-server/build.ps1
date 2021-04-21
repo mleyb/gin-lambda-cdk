6fa52db285f9d64c4e1a8df17c4b37feada4d789
Mark Leybourne <mleyb@users.noreply.github.com>
2021-04-21 17:53:57 +0100

    Update

--- functions/gin-server/build.ps1
+++ functions/gin-server/build.ps1
@@ -5,10 +5,10 @@ $env:GOOS = "linux"
 $env:GOARCH = "amd64"
 $env:CGO_ENABLED = "0"
 
-go get -v
+#go get -v
 
 go build -v -o bin\main .
 
 # TODO - sort this right out
-#~\Go\bin\build-lambda-zip.exe -o main.zip main
+~\Go\bin\build-lambda-zip.exe -output main.zip bin\main
 
---
+++ functions/gin-server/go.mod
@@ -0,0 +1,9 @@
+module github.com/mleyb/func
+
+go 1.16
+
+require (
+	github.com/aws/aws-lambda-go v1.23.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.10.0
+	github.com/gin-gonic/gin v1.7.1
+)
