3c7fbcf81d21e4e21bede39f4d612ec457c6047b
Mark Leybourne <mleyb@users.noreply.github.com>
2021-04-21 16:55:04 +0100

    Add files via upload

---
+++ functions/gin-server/build.ps1
@@ -0,0 +1,14 @@
+#$env:GO111MODULE = "on"
+#go get -u github.com/aws/aws-lambda-go/cmd/build-lambda-zip
+
+$env:GOOS = "linux"
+$env:GOARCH = "amd64"
+$env:CGO_ENABLED = "0"
+
+go get -v
+
+go build -v -o main .
+
+# TODO - sort this right out
+#~\Go\bin\build-lambda-zip.exe -o main.zip main
+
---
+++ infra/index.ts
@@ -0,0 +1,48 @@
+import lambda = require("@aws-cdk/aws-lambda")
+import apigw = require("@aws-cdk/aws-apigateway")
+import cdk = require("@aws-cdk/core")
+import assets = require("@aws-cdk/aws-s3-assets")
+import path = require("path")
+
+export class GoAPILambdaStack extends cdk.Stack {
+  constructor(app: cdk.App, id: string) {
+    super(app, id)
+
+    // The following Golang example defines an directory
+    // asset which is archived as a .zip file and uploaded to
+    // S3 during deployment.
+    // See https://docs.aws.amazon.com/cdk/api/latest/docs/aws-s3-assets-readme.html
+    const myLambdaAsset = new assets.Asset(
+      // @ts-ignore - this expects Construct not cdk.Construct :thinking:
+      this,
+      "HelloGoServerLambdaFnZip",
+      {
+        path: path.join(__dirname, "../functions/gin-server"),
+      }
+    )
+
+    const lambdaFn = new lambda.Function(this, "HelloGoServerLambdaFn", {
+      code: lambda.Code.fromBucket(
+        myLambdaAsset.bucket,
+        myLambdaAsset.s3ObjectKey
+      ),
+      timeout: cdk.Duration.seconds(300),
+      runtime: lambda.Runtime.GO_1_X,
+      handler: "main",
+    })
+
+    // API Gateway
+    new apigw.LambdaRestApi(
+      // @ts-ignore - this expects Construct not cdk.Construct :thinking:
+      this,
+      "HelloGoServerLambdaFnEndpoint",
+      {
+        handler: lambdaFn,
+      }
+    )
+  }
+}
+
+const app = new cdk.App()
+new GoAPILambdaStack(app, "HelloGoServerLambdaFn")
+app.synth()
